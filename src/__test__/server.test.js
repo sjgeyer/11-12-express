b4791e4a344630a54839812622f5e97f59a5833e
Sarah Geyer <sjgeyer@comcast.net>
2018-04-30 18:02:18 -0700

    get one get all and delete functioning and passing tests

--- src/__test__/server.test.js
+++ src/__test__/server.test.js
@@ -6,21 +6,25 @@ import { startServer, stopServer } from '../lib/server';
 import Cat from '../model/cat';
 
 const apiURL = `http://localhost:${process.env.PORT}/api/cats`;
+const badApiURL = `http://localhost:${process.env.PORT}/api/INVALIDSCHEMA`;
 
-// const createMockCat = () => {
-//   return new Cat({
-//     name: faker.name.firstName(),
-//     color: faker.commerce.color(),
-//     favoriteFood: faker.random.word(),
-//     age: faker.random.number(),
-//   }).save();
-// };
+const createMockCat = () => {
+  return new Cat({
+    name: faker.name.firstName(),
+    color: faker.commerce.color(),
+    favoriteFood: faker.random.word(1),
+    age: faker.random.number(),
+  }).save();
+};
+
+beforeAll(startServer);
+afterAll(() => {
+  stopServer();
+  Cat.remove({});
+});
 
 describe('/api/cats', () => {
-  beforeAll(startServer);
-  afterAll(stopServer);
-  afterEach(() => Cat.remove({}));
-
+  let testId = null;
   describe('POST /api/cats', () => {
     test('should respond with 200 status', () => {
       const catToPost = {
@@ -37,6 +41,7 @@ describe('/api/cats', () => {
           expect(res.body.color).toEqual(catToPost.color);
           expect(res.body.favoriteFood).toEqual(catToPost.favoriteFood);
           expect(res.body.age).toEqual(catToPost.age);
+          testId = res.body._id;
         });
     });
     test('should respond with 400 status', () => {
@@ -52,5 +57,71 @@ describe('/api/cats', () => {
           expect(res.status).toEqual(400);
         });
     });
+  });
+
+  describe('GET ONE /api/cats', () => {
+    test('should respond with 200 status', () => {
+      let testCat = null;
+      return createMockCat()
+        .then((cat) => {
+          testCat = cat;
+          return superagent.get(`${apiURL}/${cat._id}`);
+        })
+        .then((res) => {
+          expect(res.status).toEqual(200);
+          expect(res.body.name).toEqual(testCat.name);
+          expect(res.body.color).toEqual(testCat.color);
+          expect(res.body.favoriteFood).toEqual(testCat.favoriteFood);
+          expect(res.body.age).toEqual(testCat.age);
+        });
+    });
+    test('should respond with 404 if there is no note', () => {
+      return superagent.get(`${apiURL}/INVALIDID`)
+        .then(Promise.reject)
+        .catch((res) => {
+          expect(res.status).toEqual(404);
+        });
+    });
+  });
+
+  describe('GET ALL /api/cats', () => {
+    test('should respond with 200 status', () => {
+      return superagent.get(`${apiURL}`)
+        .then((res) => {
+          expect(Array.isArray(res.body)).toEqual(true);
+          expect(res.status).toEqual(200);
+        });
+    });
+    test('should respond with 404 status', () => {
+      return superagent.get(`${badApiURL}`)
+        .then(Promise.reject)
+        .catch((res) => {
+          expect(res.status).toEqual(404);
+        });
+    });
+  });
+
+  describe('DELETE /api/cats', () => {
+    test('should respond with 204 status', () => {
+      return superagent.del(`${apiURL}/${testId}`)
+        .then((res) => {
+          expect(res.status).toEqual(204);
+          expect(res.body).toEqual({});
+        });
+    });
+    test('should respond with 404 if there is no note', () => {
+      return superagent.del(`${apiURL}/INVALIDID`)
+        .then(Promise.reject)
+        .catch((res) => {
+          expect(res.status).toEqual(404);
+        });
+    });
+    // test('should respond with 400 if there is no id', () => {
+    //   return superagent.del(`${apiURL}/`)
+    //     .then(Promise.reject)
+    //     .catch((res) => {
+    //       expect(res.status).toEqual(400);
+    //     });
+    // });
   });
 });
--- src/route/cat-router.js
+++ src/route/cat-router.js
@@ -28,26 +28,67 @@ catRouter.post('/api/cats', jsonParser, (req, res) => {
   return undefined;
 });
 
+catRouter.get('/api/cats', (req, res) => {
+  logger.log(logger.INFO, 'GET ALL: processing a request');
+  Cat.find()
+    .then((cats) => {
+      logger.log(logger.INFO, 'GET ALL: 200 status');
+      return res.json(cats);
+    })
+    .catch((error) => {
+      logger.log(logger.ERROR, '__GET_ONE_ERROR__: 500 status');
+      logger.log(logger.ERROR, error);
+      return res.sendStatus(500);
+    });
+});
+
 catRouter.get('/api/cats/:id', (req, res) => {
-  logger.log(logger.INFO, 'GET: processing a request');
+  logger.log(logger.INFO, 'GET ONE: processing a request');
   Cat.findById(req.params.id)
     .then((cat) => {
       if (!cat) {
-        logger.log(logger.INFO, 'GET: 404 status (!cat)');
+        logger.log(logger.INFO, 'GET ONE: 404 status (!cat)');
         res.sendStatus(404);
       }
-      logger.log(logger.INFO, 'GET: 200 status');
+      logger.log(logger.INFO, 'GET ONE: 200 status');
       return res.json(cat);
     })
     .catch((error) => {
       if (error.message.toLowerCase().indexOf('cast to objectid failed') > -1) {
-        logger.log(logger.INFO, `GET: 404 status, error finding ${req.params.id}`);
+        logger.log(logger.INFO, `GET ONE: 404 status, error finding ${req.params.id}`);
         return res.sendStatus(404);
       }
-      logger.log(logger.ERROR, '__GET_ERROR__: 500 status');
+      logger.log(logger.ERROR, '__GET_ONE_ERROR__: 500 status');
       logger.log(logger.ERROR, error);
       return res.sendStatus(500);
     });
+});
+
+catRouter.delete('/api/cats/:id', (req, res) => {
+  logger.log(logger.INFO, 'DELETE: processing a request');
+  if (!req.params.id) {
+    logger.log(logger.INFO, 'DELETE: 400 status (!req.params.id)');
+    return res.sendStatus(400);
+  }
+  Cat.findByIdAndRemove(req.params.id)
+    .then((cat) => {
+      if (!cat) {
+        logger.log(logger.INFO, 'DELETE: 404 status (!cat)');
+        return res.sendStatus(404);
+      }
+      logger.log(logger.INFO, 'DELETE: 204 status');
+      return res.sendStatus(204);
+    })
+    .catch((error) => {
+      if (error.message.toLowerCase().indexOf('cast to objectid failed') > -1) {
+        logger.log(logger.INFO, `DELETE: 404 status, error finding ${req.params.id}`);
+        return res.sendStatus(404);
+      }
+      logger.log(logger.ERROR, '__DELETE_ERROR__: 500 status');
+      logger.log(logger.ERROR, error);
+      return res.sendStatus(500);
+    });
+  return undefined;
 });
 
 export default catRouter;
